cfccc60da7ae181232a5cc7fc8d7b6d962644c73
Marco <marcopalacios@micorreo.upp.edu.mx>
2022-02-14 23:02:06 -0600

    el marco puso otro hola

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -16,6 +16,8 @@ public class HolaMundo {
      */
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+	System.out.println("Hola mundo ");  //by: Marco
+
     }
     
 }
